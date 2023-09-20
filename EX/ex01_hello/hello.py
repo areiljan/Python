d10994dc962253fb7178b9d0ebf83e9c4c44399a
Artur Reiljan <arreil@taltech.ee>
2023-09-20 23:52:13 +0300

    ülesanded

--- EX/ex01_hello/hello.py
+++ EX/ex01_hello/hello.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+food = "Apple"
+if len(food) > 4:
+    print("Long apple")
+if len(food) == 5:
+    print("Longer apple")
+if len(food) < 2:
+    print("Short apple")
+else:
+    print("Ordinary apple")

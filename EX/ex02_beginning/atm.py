aae757d15a2bf15412533521f978303aa8b56715
Artur Reiljan <arreil@taltech.ee>
2023-09-20 23:49:47 +0300

    ülesanded

---
+++ EX/ex02_beginning/atm.py
@@ -0,0 +1,39 @@
+"""
+Create a machine that dispenses money using 1€, 5€, 10€, 20€, 50€ and 100€ banknotes.
+
+Given the sum, one must print out how many banknotes does it take to cover the sum. Task is to cover the sum with as little
+banknotes as possible.
+
+Example
+The sum is 72€
+We use four banknotes to cover it. The banknotes are 20€, 50€, 1€ and 1€.
+"""
+
+amount = int(input("Enter a sum: "))
+banknotes = 0
+#
+if amount % 100 > 0:
+    sajalised = amount//100
+    amount -= sajalised*100
+    banknotes += sajalised
+if amount % 50 > 0:
+    viiekumnelised = amount//50
+    amount -= viiekumnelised*50
+    banknotes += viiekumnelised
+if amount % 20 > 0:
+    kahekumnelised = amount // 20
+    amount -= kahekumnelised*20
+    banknotes += kahekumnelised
+if amount % 10 > 0:
+    kumnelised = amount // 10
+    amount -= kumnelised*10
+    banknotes += kumnelised
+if amount % 5 > 0:
+    viielised = amount // 5
+    amount -= viielised*10
+    banknotes += viielised
+else:
+    uhelised = amount
+    banknotes += uhelised
+#
+print(f"Amount of banknotes needed: {banknotes}")
---
+++ EX/ex02_beginning/maths.py
@@ -0,0 +1,108 @@
+"""EX02 Maths."""
+
+"""
+Find the average of a, b, c and d, but first the numbers must be multiplied. a multiplied by 1, b multiplied by 2,
+c multiplied by 3 and d multiplied by 4.
+After the multiplication find the average of the numbers and print it out.
+
+Example 1
+a = 0
+b = 0
+c = 0
+d = 4
+
+Output
+4
+
+Example 2
+a = 1
+b = 2
+c = 3
+d = 4
+Output
+7.5
+
+Example 2
+a = 5
+b = 0
+c = 5
+d = 1
+
+Output
+6
+
+ """
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
+d = int(input("Enter the value of d: "))
+print((a + b * 2 + c * 3 + d * 4) / 4)
+
+"""
+Calculate the sum of two fractions.
+
+One fraction is x/y where x and y are numbers given as input.
+The other fraction is u/t where u and t are also numbers given as input.
+
+Find and print the sum of x/y + u/t.
+
+NB! the fraction does not have to be in the simplest form.
+NB! the answer should be given as a string and should not contain any commas.
+
+Example 1
+x = 1
+y = 3
+u = 1
+t = 3
+
+Output
+2/3 or 6/9 or 4/6
+
+Example 2
+x = 2
+y = 5
+u = 1
+t = 5
+
+Output
+3/5
+"""
+x = int(input("Enter the value of x: "))
+y = int(input("Enter the value of y: "))
+u = int(input("Enter the value of u: "))
+t = int(input("Enter the value of t: "))
+print(str(x*t + u*y) + "/" + str(y*t))
+
+"""
+Calculate and print how many hours are needed per week with given ECTS and amount of weeks, if each ECTS is 26 hours.
+
+If it is not possible, print out -1.
+
+Example 1
+ects = 30
+weeks = 12
+
+Output
+65
+
+Example 2
+ects = 1
+weeks = 1
+
+output
+26
+
+Example 3
+ects = 1
+weeks = 0
+
+output
+-1
+"""
+ects = int(input("Enter the amount of ECTS: "))
+weeks = int(input("Enter the number of weeks: "))
+if weeks != 0:
+    tulemus = ects * 26 / weeks
+else:
+    tulemus = -1
+print(tulemus)

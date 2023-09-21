f463f8180a2800ff398a78eeb5189845d4b346eb
Artur Reiljan <arreil@taltech.ee>
2023-09-21 16:26:47 +0300

    ülesanded

--- EX/ex02_beginning/atm.py
+++ EX/ex02_beginning/atm.py
@@ -12,19 +12,19 @@ We use four banknotes to cover it. The banknotes are 20€, 50€, 1€ and 1€
 amount = int(input("Enter a sum: "))
 banknotes = 0
 #
-if amount / 100 > 1:
+if amount / 100 >= 1:
     banknotes += (amount // 100)
     amount -= ((amount // 100) * 100)
-if amount / 50 > 1:
+if amount / 50 >= 1:
     banknotes += (amount // 50)
     amount -= ((amount // 50) * 50)
-if amount / 20 > 1:
+if amount / 20 >= 1:
     banknotes += (amount // 20)
     amount -= ((amount // 20) * 20)
-if amount / 10 > 1:
+if amount / 10 >= 1:
     banknotes += (amount // 10)
     amount -= ((amount // 10) * 10)
-if amount / 5 > 1:
+if amount / 5 >= 1:
     banknotes += (amount // 5)
     amount -= ((amount // 5) * 5)
 banknotes += amount

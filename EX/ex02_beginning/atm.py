d54414ba385c609e9c4831a0d4561f4c0bb73508
Artur Reiljan <arreil@taltech.ee>
2023-09-21 14:01:18 +0300

    ülesanded

--- EX/ex02_beginning/atm.py
+++ EX/ex02_beginning/atm.py
@@ -12,28 +12,21 @@ We use four banknotes to cover it. The banknotes are 20€, 50€, 1€ and 1€
 amount = int(input("Enter a sum: "))
 banknotes = 0
 #
-if amount % 100 > 0:
-    sajalised = amount // 100
-    amount -= sajalised * 100
-    banknotes += sajalised
-if amount % 50 > 0:
-    viiekumnelised = amount // 50
-    amount -= viiekumnelised * 50
-    banknotes += viiekumnelised
-if amount % 20 > 0:
-    kahekumnelised = amount // 20
-    amount -= kahekumnelised * 20
-    banknotes += kahekumnelised
-if amount % 10 > 0:
-    kumnelised = amount // 10
-    amount -= kumnelised * 10
-    banknotes += kumnelised
-if amount % 5 > 0:
-    viielised = amount // 5
-    amount -= viielised * 10
-    banknotes += viielised
-else:
-    uhelised = amount
-    banknotes += uhelised
-#
+if amount / 100 > 1:
+    banknotes += (amount // 100)
+    amount -= ((amount // 100) * 100)
+if amount / 50 > 1:
+    banknotes += (amount // 50)
+    amount -= ((amount // 50) * 50)
+if amount / 20 > 1:
+    banknotes += (amount // 20)
+    amount -= ((amount // 20) * 20)
+if amount / 10 > 1:
+    banknotes += (amount // 10)
+    amount -= ((amount // 10) * 10)
+if amount / 5 > 1:
+    banknotes += (amount // 5)
+    amount -= ((amount // 5) * 5)
+banknotes += amount
+
 print(f"Amount of banknotes needed: {banknotes}")

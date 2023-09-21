40b4820629585a428d9b5870aabfa934dace8572
Artur Reiljan <arreil@taltech.ee>
2023-09-21 14:22:05 +0300

    ülesanded

--- EX/ex02_beginning/maths.py
+++ EX/ex02_beginning/maths.py
@@ -102,7 +102,7 @@ output
 ects = int(input("Enter the amount of ECTS: "))
 weeks = int(input("Enter the number of weeks: "))
 
-if weeks > 0 and (weeks * 168) > (ects * 26) and ects > 0:
+if weeks > 0 and (weeks * 168) >= (ects * 26) and ects > 0:
     print(int(ects * 26 / weeks))
 else:
     print(-1)

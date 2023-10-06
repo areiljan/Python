183d0ca9a81b5b2635e8eac143b024794bce6fc9
Artur Reiljan <arreil@taltech.ee>
2023-10-06 10:11:21 +0300

    ülesanded

--- TK/tk2/exam.py
+++ TK/tk2/exam.py
@@ -1,9 +1,9 @@
 """Tunnikontroll 2"""
 
 def middle_value(arv1 :int, arv2: int, arv3: int):
-    if arv1 > arv2 > arv3 or arv3 > arv2 > arv 1:
+    if arv1 > arv2 > arv3 or arv3 > arv2 > arv1:
         return arv2
-    elif arv2 > arv3 > arv1 or arv1 > arv3 > arv 2:
+    elif arv2 > arv3 > arv1 or arv1 > arv3 > arv2:
         return arv3
     elif arv3 > arv1 > arv2 or arv2 > arv1 > arv3
         return arv1

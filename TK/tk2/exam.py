a69c64eafb7a5e96c6464e88ebc800043c5b4b94
Artur Reiljan <arreil@taltech.ee>
2023-10-06 10:41:26 +0300

    ülesanded

--- TK/tk2/exam.py
+++ TK/tk2/exam.py
@@ -35,3 +35,54 @@ def lucky_guess(arv: int):
             return True
     return False
 
+
+def without_end(s: str) -> str:
+    """
+    Given a string, return a version without the first and last char, so "Hello" yields "ell".
+
+    The string length will be at least 2.
+
+    without_end('Hello') → 'ell'
+    without_end('java') → 'av'
+    without_end('coding') → 'odin'
+
+    :param s: String
+    :return: String without first and last char.
+    """
+    if len(s) >= 2:
+        s = s.strip(s[0])
+        s = s.strip(s[len(s) - 1])
+        return s
+
+
+def non_decreasing_list(nums: list) -> bool:
+    """
+    Given a list of numbers.
+
+    If given list is a non-decreasing list, return True, otherwise False.
+    Non-decreasing means every next element in the list must not be smaller than the previous one.
+
+    non_decreasing_list([0, 1, 2, 3, 98]) => True
+    non_decreasing_list([50, 49]) => False
+    non_decreasing_list([12, 12]) => True
+
+    :param nums:
+    :return:
+    """
+    pass
+
+
+def max_duplicate(nums: list) -> int | None:
+    """
+    Return the largest element which has at least one duplicate.
+
+    If no element has duplicate element (an element with the same value), return None.
+
+    max_duplicate([1, 2, 3]) => None
+    max_duplicate([1, 2, 2]) => 2
+    max_duplicate([1, 2, 2, 1, 1]) => 2
+
+    :param nums: List of integers
+    :return: Maximum element with duplicate. None if no duplicate found.
+    """
+    pass

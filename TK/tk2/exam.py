ece3d56f639138807f43878719fbe6db4221e1a8
Artur Reiljan <arreil@taltech.ee>
2023-10-06 10:18:49 +0300

    ülesanded

--- TK/tk2/exam.py
+++ TK/tk2/exam.py
@@ -1,11 +1,19 @@
 """Tunnikontroll 2"""
 
+
 def middle_value(arv1 :int, arv2: int, arv3: int):
-    if arv1 > arv2 > arv3 or arv3 > arv2 > arv1:
+    """Leiab keskmise väärtuse"""
+    if min(arv1, arv2, arv3) == arv1 and max(arv1, arv2, arv3) == arv3:
         return arv2
-    elif arv2 > arv3 > arv1 or arv1 > arv3 > arv2:
+    elif min(arv1, arv2, arv3) == arv2 and max(arv1, arv2, arv3) == arv1:
         return arv3
-    elif arv3 > arv1 > arv2 or arv2 > arv1 > arv3:
+    elif min(arv1, arv2, arv3) == arv3 and max(arv1, arv2, arv3) == arv2:
+        return arv1
+    elif max(arv1, arv2, arv3) == arv1 and min(arv1, arv2, arv3) == arv3:
+        return arv2
+    elif max(arv1, arv2, arv3) == arv2 and min(arv1, arv2, arv3) == arv1:
+        return arv3
+    elif max(arv1, arv2, arv3) == arv3 and min(arv1, arv2, arv3) == arv2:
         return arv1
     else:
         return None

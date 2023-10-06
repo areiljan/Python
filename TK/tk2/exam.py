f36394237ad2f0515b57a2c273c07462bcda076e
Artur Reiljan <arreil@taltech.ee>
2023-10-06 10:11:51 +0300

    ülesanded

--- TK/tk2/exam.py
+++ TK/tk2/exam.py
@@ -5,5 +5,5 @@ def middle_value(arv1 :int, arv2: int, arv3: int):
         return arv2
     elif arv2 > arv3 > arv1 or arv1 > arv3 > arv2:
         return arv3
-    elif arv3 > arv1 > arv2 or arv2 > arv1 > arv3
+    elif arv3 > arv1 > arv2 or arv2 > arv1 > arv3:
         return arv1

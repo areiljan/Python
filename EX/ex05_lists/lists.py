e5d49787490539cd94f4bffcd4884bc9d99f0375
Artur Reiljan <arreil@taltech.ee>
2023-10-14 10:49:22 +0300

    ülesanded

---
+++ EX/ex05_lists/lists.py
@@ -0,0 +1,49 @@
+"""Phone inventory."""
+
+
+def list_of_phones(all_phones: str) -> list:
+    """
+    Return list of phones.
+
+    The input string contains of phone brands and models, separated by comma.
+    Both the brand and the model do not contain spaces (both are one word).
+
+    "Google Pixel,Honor Magic5,Google Pixel" => ["Google Pixel', 'Honor Magic5', 'Google Pixel"]
+    """
+    phones_list = all_phones.split(",")
+    return phones_list
+
+
+def phone_brands(all_phones: str) -> list:
+    """
+    Return list of unique phone brands.
+
+    The order of the elements should be the same as in the input string (first appearance).
+
+    "Google Pixel,Honor Magic5,Google Pixel" => ["Google", "Honor"]
+    """
+    phones_list = list_of_phones(all_phones)
+    brands_list = []
+    for element in phones_list:
+        brand = element.split(" ")
+        if brand[0] not in brands_list:
+            brands_list.append(brand[0])
+    return brands_list
+
+
+def phone_models(all_phones: str) -> list:
+    """
+    Return list of unique phone models.
+
+    The order of the elements should be the same as in the input string (first appearance).
+
+    "Honor Magic5,Google Pixel,Honor Magic4" => ['Magic5', 'Pixel', 'Magic4']
+    """
+    phones_list = list_of_phones(all_phones)
+    models_list = []
+    for element in phones_list:
+        if element not in models_list:
+            models_list.append(element)
+    return models_list
+
+

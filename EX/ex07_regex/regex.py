8a6c777bee6ea7dbfc924f34b289bd4fd6a41e56
Artur Reiljan <arreil@taltech.ee>
2023-11-03 14:24:07 +0200

    ülesanded

--- EX/ex07_regex/regex.py
+++ EX/ex07_regex/regex.py
@@ -33,7 +33,7 @@ def find_words_with_vowels(text: str) -> list:
     :param text: given string to find words from
     :return: list of words that start with a vowel found in given string
     """
-    pattern = r'[AEIOUÄÖÜÕ][a-zäöüõ]*'
+    pattern = r'[AEIOUÄÖÜÕ][a-zäöüõ]{2,}'
     return re.findall(pattern, text)
 
 

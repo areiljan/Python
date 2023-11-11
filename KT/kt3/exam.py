26900b2ace7ce936d19581ee3b57b2e7fdcda4a6
Artur Reiljan <arreil@taltech.ee>
2023-11-11 14:05:28 +0200

    ülesanded

---
+++ KT/kt3/exam.py
@@ -0,0 +1,96 @@
+"""KT3."""
+
+
+def last_to_first(s):
+    """
+    Move last symbol to the beginning of the string.
+
+    last_to_first("ab") => "ba"
+    last_to_first("") => ""
+    last_to_first("hello") => "ohell"
+    """
+    if s == "":
+        return ""
+    else:
+        return s[-1] + s[:-1]
+
+
+def only_one_pair(numbers: list) -> bool:
+    """
+    Whether the list only has one pair.
+
+    Function returns True, if the list only has one pair (two elements have the same value).
+    In other cases:
+     there are no elements with the same value
+     there are more than 2 elements with the same value
+     there are several pairs
+    returns False.
+
+    only_one_pair([1, 2, 3]) => False
+    only_one_pair([1]) => False
+    only_one_pair([1, 2, 3, 1]) => True
+    only_one_pair([1, 2, 1, 3, 1]) => False
+    only_one_pair([1, 2, 1, 3, 1, 2]) => False
+    """
+    pass
+
+
+def swap_dict_keys_and_value_lists(d: dict) -> dict:
+    """
+    Swap keys and values in dict.
+
+    Values are lists.
+    Every element in this list should be a key,
+    and current key will be a value for the new key.
+    Values in the result are lists.
+
+    Every list in input dict has at least 1 element.
+    The order of the values in the result dict is not important.
+
+    swap_dict_keys_and_value_lists({"a": ["b", "c"]}) => {"b": ["a"], "c": ["a"]}
+    swap_dict_keys_and_value_lists({1: [2, 3], 4: [2, 5]}) => {2: [1, 4], 3: [1], 5: [4]}
+    swap_dict_keys_and_value_lists({}) => {}
+    swap_dict_keys_and_value_lists({1: [2]}) => {2: [1]}
+    """
+    pass
+
+
+def substring(s: str, count: int) -> str:
+    """
+    Return first part of string with length of count.
+
+    Function should be recursive, loops (for/while) are not allowed!
+    count <= len(string)
+
+    assert substring("hello", 2) == "he"
+    assert substring("hi", 2) == "hi"
+    assert substring("house", -1) == ""
+    assert substring("house", 0) == ""
+
+    :param s: input string.
+    :param count: int, count <= len(string).
+    :return: first count symbols from string.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    assert last_to_first("ab") == "ba"
+    assert last_to_first("") == ""
+    assert last_to_first("hello") == "ohell"
+
+    assert only_one_pair([1, 2, 3]) is False
+    assert only_one_pair([1]) is False
+    assert only_one_pair([1, 2, 3, 1]) is True
+    assert only_one_pair([1, 2, 1, 3, 1]) is False
+    assert only_one_pair([1, 2, 1, 3, 1, 2]) is False
+
+    assert swap_dict_keys_and_value_lists({"a": ["b", "c"]}) == {"b": ["a"], "c": ["a"]}
+    assert swap_dict_keys_and_value_lists({1: [2, 3], 4: [2, 5]}) == {2: [1, 4], 3: [1], 5: [4]}  # or {2: [4, 1], 3: [1], 5: [4]}
+    assert swap_dict_keys_and_value_lists({}) == {}
+    assert swap_dict_keys_and_value_lists({1: [2]}) == {2: [1]}
+
+    assert substring("hello", 2) == "he"
+    assert substring("hello", -1) == ""
+    assert substring("", 0) == ""
+    assert substring("world", 5) == "world"

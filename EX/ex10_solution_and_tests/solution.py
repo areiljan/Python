a4ad462dd9ebe3132969bc75c87e2b1b828dc133
Artur Reiljan <arreil@taltech.ee>
2023-11-15 18:17:25 +0200

    ülesanded

---
+++ EX/ex10_solution_and_tests/solution.py
@@ -0,0 +1,49 @@
+"""Solution to be tested."""
+
+
+def students_study(time: int, coffee_needed: bool) -> bool:
+    """
+    Return True if students study in given circumstances.
+
+    (19, False) -> True
+    (1, True) -> False.
+    """
+    if 18 <= time <= 24:
+        coffee_needed = True
+    elif  5 <= time <= 17:
+        coffee_needed = True
+    else:
+        coffee_needed = False
+    return coffee_needed
+
+
+def lottery(a: int, b: int, c: int) -> int:
+    """
+    Return Lottery victory result 10, 5, 1, or 0 according to input values.
+
+    (5, 5, 5) -> 10
+    (2, 2, 1) -> 0
+    (2, 3, 1) -> 1
+    """
+    if a == b == c == 5:
+        return 10
+    elif a == b == c:
+        return 5
+    elif a != b and a != c:
+        return 1
+    else:
+        return 0
+
+
+
+def fruit_order(small_baskets: int, big_baskets: int, ordered_amount: int) -> int:
+    """
+    Return number of small fruit baskets if it's possible to finish the order, otherwise return -1.
+
+    (4, 1, 9) -> 4
+    (3, 1, 10) -> -1
+    """
+    if ((ordered_amount % big_baskets) / small_baskets) <= 1:
+        return small_baskets
+    else:
+        return -1

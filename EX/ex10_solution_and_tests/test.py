b848283fd756648c5e7422f917022f20ffcb62f7
Artur Reiljan <arreil@taltech.ee>
2023-11-17 18:16:02 +0200

    ülesanded

--- EX/ex10_solution_and_tests/test.py
+++ EX/ex10_solution_and_tests/test.py
@@ -2,20 +2,20 @@ import pytest
 from solution import students_study as study
 
 def test_students_study_evening_false():
-    assert study(19, False) == True
+    assert study(18, False) == True
 
 def test_students_study_evening_true():
-    assert study(19, True) == True
+    assert study(24, True) == True
 
 def test_students_study_morning_false():
-    assert study(10, False) == False
+    assert study(5, False) == False
 
 def test_students_study_morning_true():
-    assert study(10, True) == True
+    assert study(17, True) == True
 
 def test_students_study_night_false():
-    assert study(3, False) == False
+    assert study(1, False) == False
 
 def test_students_study_night_true():
-    assert study(3, True) == False
+    assert study(4, True) == False
 

c73051738a5cf7600097b52e164ed2b2a54f41b3
Artur Reiljan <arreil@taltech.ee>
2023-11-18 16:42:43 +0200

    ülesanded

--- EX/ex10_solution_and_tests/test.py
+++ EX/ex10_solution_and_tests/test.py
@@ -1,24 +1,6 @@
 import pytest
 from solution import students_study as study
 
-def test_students_study_evening_edge_false():
-    assert study(18, False) == True
-
-def test_students_study_evening_edge_true():
-    assert study(24, True) == True
-
-def test_students_study_morning_edge_false():
-    assert study(5, False) == False
-
-def test_students_study_morning_edge_true():
-    assert study(17, True) == True
-
-def test_students_study_night_edge_false():
-    assert study(1, False) == False
-
-def test_students_study_night_edge_true():
-    assert study(4, True) == False
-
 def test_students_study_evening_false():
     assert study(20, False) == True
 
@@ -36,6 +18,24 @@ def test_students_study_night_false():
 
 def test_students_study_night_true():
     assert study(3, True) == False
+
+def test_students_study_evening_edge_false():
+    assert study(18, False) == True
+
+def test_students_study_evening_edge_true():
+    assert study(24, True) == True
+
+def test_students_study_morning_edge_false():
+    assert study(5, False) == False
+
+def test_students_study_morning_edge_true():
+    assert study(17, True) == True
+
+def test_students_study_night_edge_false():
+    assert study(1, False) == False
+
+def test_students_study_night_edge_true():
+    assert study(4, True) == False
 
 def test_students_study_negative_time_true():
     assert study(-1, True) == False

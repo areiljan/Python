dee38664f1fa485a5e21ce40fb1370b49c8a9750
Artur Reiljan <arreil@taltech.ee>
2023-12-04 12:47:41 +0200

    ülesanded

---
+++ EX/ex12_music/music.py
@@ -0,0 +1,169 @@
+"""Music."""
+
+
+class Note:
+    """
+    Note class.
+
+    Every note has a name and a sharpness or alteration (supported values: "", "#", "b").
+    """
+    def __init__(self, note: str):
+        """Initialize the class.
+
+        To make the logic a bit easier it is recommended to normalize the notes, that is, choose a sharpness
+        either '#' or 'b' and use it as the main, that means the notes will be either A, A#, B, B#, C etc or
+        A Bb, B, Cb, C.
+        Note is a single alphabetical letter which is always uppercase.
+        NB! Ab == Z#
+        """
+        self.note = self.normalize(note)
+
+    def normalize(self, note: str):
+        note = note.upper()
+        if len(note) == 1:
+            return note
+
+        if note[0] == "Z":
+            next_char = "A"
+        else:
+            next_unicode_value = ord(note[0]) + 1  # Increment the Unicode value to get the next character
+            next_char = chr(next_unicode_value)
+
+        if note[1] == 'B':
+            normalized_note = note[0] + "b"
+        elif note[1] == '#':
+            normalized_note = next_char + "b"
+
+        return normalized_note
+
+    def __repr__(self) -> str:
+        """
+        Representation of the Note class.
+
+        Return: <Note: [note]> where [note] is the note_name + sharpness if the sharpness is given, that is not "".
+        Repr should display the original note and sharpness, before normalization.
+        """
+        return f"<Note: {self.note}>"
+
+    def __eq__(self, other):
+        """
+        Compare two Notes.
+
+        Return True if equal otherwise False. Used to check A# == Bb or Ab == Z#
+        """
+        if other is None or self.note is None:
+            return False
+        else:
+            if len(other) == 1 and len(self.note) == 1:
+                return other.lower() == self.note.lower()
+            if len(other) == 2:
+                if (other[1] == "b" and self.note[1] == "b" or other[1] == "#" and self.note[1] == "#"):
+                    return other[0].lower() == self.note[0].lower()
+            return False
+
+
+
+class NoteCollection:
+    """NoteCollection class."""
+
+    def __init__(self):
+        """
+        Initialize the NoteCollection class.
+
+        You will likely need to add something here, maybe a dict or a list?
+        """
+        self.note_collection = []
+    def add(self, note: Note) -> None:
+        """
+        Add note to the collection.
+
+        Check that the note is an instance of Note, if it is not, raise the built-in TypeError exception.
+
+        :param note: Input object to add to the collection
+        """
+        if not isinstance(note, Note):
+            raise TypeError("Input 'note' must be an instance of the Note class")
+
+        self.note_collection.append(note)
+
+
+    def pop(self, note: str) -> Note | None:
+        """
+        Remove and return previously added note from the collection by its name.
+
+        If there are no elements with the given name, do not remove anything and return None.
+
+        :param note: Note to remove
+        :return: The removed Note object or None.
+        """
+        return None
+
+    def extract(self) -> list[Note]:
+        """
+        Return a list of all the notes from the collection and empty the collection itself.
+
+        Order of the list must be the same as the order in which the notes were added.
+
+        Example:
+          collection = NoteCollection()
+          collection.add(Note('A'))
+          collection.add(Note('C'))
+          collection.extract() # -> [<Note: A>, <Note: C>]
+          collection.extract() # -> []
+
+        In this example, the second time we use .extract() the output list is empty because we already extracted everything.
+
+        :return: A list of all the notes that were previously in the collection.
+        """
+        return []
+
+    def get_content(self) -> str:
+        """
+        Return a string that gives an overview of the contents of the collection.
+
+        Example:
+          collection = NoteCollection()
+          collection.add(Note('C#'))
+          collection.add(Note('Lb'))
+          print(collection.get_content())
+
+        Output in console:
+           Notes:
+            * C#
+            * Lb
+
+        The notes must be sorted alphabetically by name and then by sharpness, that is A, A#, B, Cb, C and so on.
+        Recommendation: Use normalized note names, not just the __repr__()
+
+        :return: Content as a string
+        """
+        return ''
+
+if __name__ == '__main__':
+    note_one = Note('a') # yes, lowercase
+    note_two = Note('C')
+    note_three = Note('Eb')
+    collection = NoteCollection()
+
+    print(note_one) # <Note: A>
+    print(note_three) # <Note: Eb>
+
+    collection.add(note_one)
+    collection.add(note_two)
+
+    print(collection.get_content())
+    # Notes:
+    #   * A
+    #   * C
+
+    print(collection.extract()) # [<Note: A>,<Note: C>]
+    print(collection.get_content())
+    # Notes:
+    #  Empty
+
+    collection.add(note_one)
+    collection.add(note_two)
+    collection.add(note_three)
+
+    print(collection.pop('a') == note_one)  # True
+    print(collection.pop('Eb') == note_three)  # True

bf73e72709babed635a6e537ad8a90feae9fef39
Artur Reiljan <arreil@taltech.ee>
2024-01-04 13:42:16 +0200

    ülesanded

--- EXAM/exam00/exam.py
+++ EXAM/exam00/exam.py
@@ -78,9 +78,10 @@ def get_names_from_results(results_string: str, min_result: int) -> list:
     new_list = []
     for names_and_scores in names_and_scores_list:
         score = re.findall(pattern, names_and_scores)
-        name = re.sub(new_pattern,'', names_and_scores)
-        if int(score[0]) >= min_result:
-            new_list.append(name)
+        name = re.sub(pattern,'', names_and_scores)
+        if score != []:
+            if int(score[0]) >= min_result:
+                new_list.append(name)
     return new_list
 
 def tic_tac_toe(game: list) -> int:

be849932d59fda2a3997035eff2ac1bfa015b6a5
Artur Reiljan <arreil@taltech.ee>
2024-01-04 19:26:57 +0200

    ülesanded

--- EXAM/exam00/exam.py
+++ EXAM/exam00/exam.py
@@ -269,8 +269,8 @@ def get_ordered_students(students: list) -> list:
 
     credit points (higher first), average_grade (higher first), name (a to z).
     """
-    pass
-
+    sorted_students = sorted(students, key=lambda student: -student.credit_points, -student.average_grade, student.name, reverse=True)
+    return sorted_students
 
 class Room:
     """Room."""
